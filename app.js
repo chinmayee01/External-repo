974cfbf79a7d9775f67801bb4c35f2e3ca278e44
chinmayee <chinmapa@in.ibm.com>
2015-07-15 16:36:56 +0530

    priya changes aswell as one image added

--- app.js
+++ app.js
@@ -153,13 +153,13 @@ app.get('/',function (req, res) {
 app.use(express.static(__dirname + '/public'));
 
 
-//// get the app environment from Cloud Foundry
-//var appEnv = cfenv.getAppEnv();
-//// start server on the specified port and binding host
-//app.listen(appEnv.port, appEnv.bind, function() {
-//  // print a message when the server starts listening
-//  console.log("server starting on " + appEnv.url);
-//});
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+// start server on the specified port and binding host
+app.listen(appEnv.port, appEnv.bind, function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
 
 app.listen(port);
 console.log("App listening on port " + port);
